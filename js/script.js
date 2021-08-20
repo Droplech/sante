05b3fbfbc5a40eb5a35206c4206d4f02ee242d8e
Maxim <53229586+Droplech@users.noreply.github.com>
2021-08-20 16:57:30 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -18,6 +18,59 @@ $(document).ready(function(){
         $(this).find('.li_open_menu').fadeOut()
     }) 
 
+    $('.featured_items_product').hover(function(){
+        $(this).find('.quick_view').css({
+            'right':'-45px',
+            'transition':'0.3s'
+        })
+        $(this).find('.add_cart_btn').css({
+            'transform': 'translateX(0%)',
+            'transition':'0.3s'
+        })
+        $(this).find('.price').css({
+            'transform': 'translateX(210%)',
+            'transition':'0.3s'
+        })
+
+    },function(){
+        $(this).find('.quick_view').css({
+            'right':'-83px',
+            'transition':'0.3s'
+        })
+        $(this).find('.add_cart_btn').css({
+            'transform': 'translateX(-100%)',
+            'transition':'0.3s'
+        })
+        $(this).find('.price').css({
+            'transform': 'translateX(0%)',
+            'transition':'0.3s'
+        })
+    })
+
+    new Swiper('.block_preview_slider',{
+        navigation: {
+            nextEl: '#preview-arrow-next',
+            prevEl: '#preview-arrow-prev',
+        },
+        autoplay: {
+            delay: 5000,
+        },
+        
+        loop: true,
+    })
+    new Swiper('.featured_slider',{
+        navigation: {
+            nextEl: '#featured-next',
+            prevEl: '#featured-prev',
+        },
+        slidesPerView: 4,
+        spaceBetween: 30,
+        autoplay: {
+            delay: 5000,
+        },
+        
+        
+    })
 
 
 
